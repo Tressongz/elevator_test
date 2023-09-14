6cb9c9ca8b2315c41c4f22b5304fbcbfb714035c
Tressongz <hodrando@gmail.com>
2023-09-14 23:24:01 +0300

    Added basic logic transport elevators

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <div class="house">
-    <div class="shaft" v-for="shaft in elevators">
-<!--      Добавить ключ-id, по которому будет происходить управление лифтом, так же надо научиться брать значение bottom -->
-      <Elevator id="elevator"/>
-      <Floor v-for="floor in floors"/>
+  <div class="wrapper">
+    <div class="house">
+      <div
+          class="shaft"
+          v-for="shaft in elevators"
+          :key="shaft.id"
+      >
+        <Elevator v-bind:id="`elevator_${shaft.id}`"/>
+        <Floor v-for="floor in floors"/>
+      </div>
     </div>
-    <button @click="changeFloorUp">Подними</button>
-    <button @click="changeFloorDown">Опусти</button>
-<!--    <div class="panel"></div> Тут будут кнопки для вызова лифта-->
+    <div class="panel">
+      <button
+          @click="elevatorCall(floor)"
+          class="elevatorButton"
+          v-for="floor in floors"
+          :key="floor"
+      >
+        {{ floor }}
+      </button>
+    </div>
+    <div class="queue">
+      Этажи в работе: {{ queueCalls }}
+    </div>
   </div>
 </template>
 
@@ -15,32 +30,71 @@
 import Elevator from "./components/Elevator.vue";
 import Floor from "./components/Floor.vue";
 import {ref} from "vue";
-//Добавить массив объектов-лифтом (айди, высота..)
-const elevators = ref(2)
+
+const elevators = ref([
+    {id: 1, floor: 1, isFree: true},
+    {id: 2, floor: 1, isFree: true},
+    {id: 3, floor: 1, isFree: true}
+])
+const queueCalls = ref([])
+const floorsWorking = ref([])
 const floors = ref(6)
-const currentFloor = ref(1)
-// Добавить плавное перемещение лифта.
-function changeFloorUp() {
-  setTimeout(() => {
-    currentFloor.value += 1
-    let height = 111 * currentFloor.value - 111 + 'px'
-    document.getElementById('elevator').style.bottom = height
-  }, 1000)
+
+function choiceElevator(floor) { //isTrue
+  let floors = []
+  elevators.value.forEach((el) => {
+    if (el.isFree) {
+      floors.push({id: el.id, floor: Math.abs(el.floor - floor)})
+    }
+  })
+  floors.sort((a,b) => a.floor - b.floor)
+  return floors[0].id
+}
+
+function elevatorCall(floor) { //Refactoring
+  if (queueCalls.value.find(el => el === floor) === undefined && floorsWorking.value.find(el => el === floor) === undefined) {
+    queueCalls.value.push(floor)
+  }
+    if (elevators.value.find(el => el.isFree === true)) {
+      let elevator = choiceElevator(queueCalls.value[0])
+      let time = Math.abs(floor - elevators.value[elevator - 1].floor) * 1000
+      floorsWorking.value.push(floor)
+      moveElevator(elevator, queueCalls.value[0], time)
+      callDone()
+      setTimeout(() => {
+        // Лифт окончил работу
+        setTimeout(() => {
+          elevators.value.find(el => el.id === elevator).isFree = true
+          floorsWorking.value.shift()
+        }, 3000)
+      }, time)
+    } else {
+      setTimeout(() => {
+        elevatorCall(floor)
+      }, 3000)
+    }
 
 }
 
-function changeFloorDown() {
-  setTimeout(() => {
-    currentFloor.value -= 1
-    let height = 111 * currentFloor.value - 111 + 'px'
-    document.getElementById('elevator').style.bottom = height
-  }, 1000)
+function callDone() { // isTrue
+  queueCalls.value.shift()
+}
 
+function moveElevator(idElevator, targetFloor, time) { // isTrue
+  let height = -111 * targetFloor + 111 + 'px'
+  elevators.value.find(el => idElevator === el.id).isFree = false
+  let object = document.getElementById(`elevator_${idElevator}`)
+  object.style.transform = `translate(0, ${height})`
+  object.style.transition = `transform ${time}ms`
+  elevators.value.find(el => el.id === idElevator).floor = targetFloor
 }
 </script>
 
 
 <style lang="sass" scoped>
+.wrapper
+  display: flex
+  flex-direction: row
 .house
   display: flex
   flex-direction: row
@@ -50,4 +104,16 @@ function changeFloorDown() {
   border-left: 1px solid cornflowerblue
   border-right: 1px solid cornflowerblue
   margin-right: 10px
+.panel
+  display: flex
+  flex-direction: column-reverse
+  margin-left: 10px
+.elevatorButton
+  padding: 10px
+  border-radius: 15px
+  transition: .8s
+  cursor: pointer
+  margin: 35px 0 35px 0
+.elevatorButton:hover
+  background-color: cornflowerblue
 </style>
--- src/components/Elevator.vue
+++ src/components/Elevator.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="elevator"></div>
+  <div class="elevator move"></div>
 </template>
 
 <script setup>
@@ -14,4 +14,9 @@
   height: 110px
   background-color: limegreen
   box-sizing: content-box
+  -webkit-transition: -webkit-transform 1000ms
+  transform: translate(0, 0)
+  transition: transform 1000ms
+.elevator.move
+  transform: translate(0, 0px)
 </style>
